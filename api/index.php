537412138c42c160fd23e036a13cf0bc85b78e4c
marawan28 <marawansufwat28@gmail.com>
2026-04-24 21:26:05 +0300

    graduation 10-edit

--- api/index.php
+++ api/index.php
@@ -1,20 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
+// 1. تحميل الـ Autoloader
 require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// 2. تشغيل التطبيق (تأكد من المسار)
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// 3. معالجة الطلب
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
