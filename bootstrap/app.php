3827d23286827b796a150a2255b88127d59d5db9
marawan28 <marawansufwat28@gmail.com>
2026-05-03 22:44:48 +0300

    graduation 18:4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@
 // ============================================================
 declare(strict_types=1);
 
-
+use App\Exceptions\KickZoneExceptionHandler;
 use App\Exceptions\Handler;
 use App\Http\Middleware\{
     EnsurePhoneVerified,
