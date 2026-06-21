95de56cb18e8ecd348f2fe47b853c93a5245e01f
marawan28 <marawansufwat28@gmail.com>
2026-06-21 11:47:43 +0300

    graduation 18:90

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append([
             \App\Http\Middleware\ForceJsonResponse::class,
             \App\Http\Middleware\SetApiLocale::class,
-            \App\Http\Middleware\RoleMiddleware::class,
+            ]);
+        $middleware->alias([
             \App\Http\Middleware\EnsureUserIsOwner::class,
             \App\Http\Middleware\EnsurePhoneVerified::class,
         ]);
